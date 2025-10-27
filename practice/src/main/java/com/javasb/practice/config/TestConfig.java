3730bc9216ce34b50e4576d1fe25d52596811a8b
Hugo Angelis <hugo11angelis@hotmail.com>
2025-10-27 19:29:01 -0300

    teste no banco e correção de cotegories

--- practice/src/main/java/com/javasb/practice/config/TestConfig.java
+++ practice/src/main/java/com/javasb/practice/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.javasb.practice.entities.Category;
 import com.javasb.practice.entities.Order;
 import com.javasb.practice.entities.User;
 import com.javasb.practice.enums.OrderStatus;
+import com.javasb.practice.repositories.CategoryRepository;
 import com.javasb.practice.repositories.OrderRepository;
 import com.javasb.practice.repositories.UserRepository;
 
@@ -22,8 +24,17 @@ public class TestConfig implements CommandLineRunner {
 
   @Autowired
   private OrderRepository orderRepository;
+
+  @Autowired
+  private CategoryRepository categoryRepository;
   @Override
   public void run(String... args) throws Exception {
+
+    Category cat1 = new Category(null, "Electronics");
+    Category cat2 = new Category(null, "Books");
+    Category cat3 = new Category(null, "Computers"); 
+    categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
     User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
     User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");   
     Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID ,u1);
