97832211c745bc4d9d7704bf7c7bc7b18350e4a4
Hugo Angelis <hugo11angelis@hotmail.com>
2025-10-27 20:18:05 -0300

    order item e correção nos construtores

--- practice/src/main/java/com/javasb/practice/entities/Product.java
+++ practice/src/main/java/com/javasb/practice/entities/Product.java
@@ -29,13 +29,14 @@ public class Product  implements Serializable{
   @JoinTable(name = "tb_product_category", 
   joinColumns = @JoinColumn(name = "product_id"),
   inverseJoinColumns = @JoinColumn(name = "category_id"))
-  
+
   private Set<Category> categories = new HashSet<>();
 
   public Product() {
   }
 
   public Product(Long id, String name, String description, Double price, String img) {
+    super();
     this.id = id;
     this.name = name;
     this.description = description;
