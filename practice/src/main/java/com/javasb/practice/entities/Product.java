28ad2a81cb344a59b393776a0c409dbbb6e7d960
Hugo Angelis <hugo11angelis@hotmail.com>
2025-10-27 19:44:38 -0300

    product

--- practice/src/main/java/com/javasb/practice/entities/Product.java
+++ practice/src/main/java/com/javasb/practice/entities/Product.java
@@ -11,57 +11,73 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable{
+@Table(name = "tb_product")
+public class Product  implements Serializable{
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
-  
-  private Set<Product> products = new HashSet<>();
+  private String description;
+  private Double price;
+  private String img;
 
-  
-  public Set<Product> getProducts() {
-    return products;
+  private Set<Category> categories = new HashSet<>();
+
+  public Product() {
   }
 
-  public Category() {
-  }
-
-
-
-  public Category(Long id, String name) {
-    super();
+  public Product(Long id, String name, String description, Double price, String img) {
     this.id = id;
     this.name = name;
+    this.description = description;
+    this.price = price;
+    this.img = img;
   }
-
-
 
   public Long getId() {
     return id;
   }
 
-
-
   public void setId(Long id) {
     this.id = id;
   }
-
-
 
   public String getName() {
     return name;
   }
 
-
-
   public void setName(String name) {
     this.name = name;
   }
 
+  public String getDescription() {
+    return description;
+  }
 
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public String getImg() {
+    return img;
+  }
+
+  public void setImg(String img) {
+    this.img = img;
+  }
+
+  public Set<Category> getCategories() {
+    return categories;
+  }
 
   @Override
   public int hashCode() {
@@ -71,8 +87,6 @@ public class Category implements Serializable{
     return result;
   }
 
-
-
   @Override
   public boolean equals(Object obj) {
     if (this == obj)
@@ -81,7 +95,7 @@ public class Category implements Serializable{
       return false;
     if (getClass() != obj.getClass())
       return false;
-    Category other = (Category) obj;
+    Product other = (Product) obj;
     if (id == null) {
       if (other.id != null)
         return false;
@@ -91,5 +105,8 @@ public class Category implements Serializable{
   }
 
 
+  
 
+
+  
 }
---
+++ practice/src/main/java/com/javasb/practice/resources/ProductResource.java
@@ -0,0 +1,30 @@
+package com.javasb.practice.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javasb.practice.entities.Product;
+import com.javasb.practice.services.ProductService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping(value = "/products")
+
+public class ProductResource {
+
+  @Autowired
+  private ProductService service;
+
+  @GetMapping
+  public ResponseEntity<List<Product>> findAll(){
+    List<Product> list = service.findAll();
+    return ResponseEntity.ok().body(list);
+  }
+}
